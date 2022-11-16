0e9ff8a08d5347fe3de0127c048ccf38e86f1971
golendercaria <>
2022-11-16 19:14:03 +0100

    Initial commit

---
+++ git.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Git
+Plugin URI: https://nouslesdevs.com
+Description: Just for display current repository for developpment team
+Version: 1.0.0
+Requires at least: any
+Requires PHP: 5.6
+Author: Yann Vangampelaere
+Author URI: https://nouslesdevs.com
+License: GPLv2 or later
+Text Domain: git
+*/
+
+namespace GOL_git;
+
